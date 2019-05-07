adadbdac71990fceadf004652a4a0aab10ff7cc1
Donadeu <43960130+Donadeu@users.noreply.github.com>
2019-05-07 16:12:45 +0200

    Add files via upload

--- apartatA.cpp
+++ apartatA.cpp
@@ -1,38 +1,40 @@
 #include <cstdlib>
+#include <iomanip>
 #include <random>
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <list>
 using namespace std;
 
-void read_graph(vector<pair<int,int>> &adj){
+void read_graph(vector< pair<int,int> > &adj){
 	int a, b;
 	while(cin >> a >> b){
 		adj.push_back(make_pair(a,b));
 	}
 }
 
-void print_graph(vector<pair<int,int>> adj){
+void print_graph(vector< pair<int,int> > adj){
 	for(int i = 0; i < adj.size(); i++){
 		cout << "(" << adj[i].first <<"," << adj[i].second << ")" << endl;
 	}
 }
 
-void percolation_sites(vector<pair<int,int>> &adj, list<int> &nodes){
+void percolation_sites(vector< pair<int,int> > &adj, list<int> &nodes, float q){
 	default_random_engine generator;
-	bernoulli_distribution distribution(q);
+	std::bernoulli_distribution distribution(q);
 	
 	list<int>::iterator it;
 	for(it = nodes.begin(); it != nodes.end(); it++){
 		// probabilitat que el node falli
 		bool falla = distribution(generator);
 		if(falla){
-			if(site_per) nodes.remove(*it); // s'elimina el node
+			nodes.remove(*it); // s'elimina el node
 			// s'eliminen les seves adjacències
-			vector<pair<int,int>>::iterator it = find_if(adj.begin(), adj.end(), Cond);
-			if(it != adj.end()) adj.erase(it);
 			// o bé:
-			vector<pair<int,int>>::iterator it = find_if(adj.begin(), adj.end(), [&int x] (const pair<int,int> &p) {return x == p.first or x == p.second;});
+			int x;
+			vector< pair<int,int> >::iterator it = find_if(adj.begin(), adj.end(),(const pair<int,int> p) {return x == p.first or x == p.second;});
+		}
 	}
 }
 
@@ -40,7 +42,7 @@ bool Cond(pair<int,int> p, int x){
 	return (x == p.first or x == p.second);
 }
 
-void percolation_bonds(vector<pair<int,int>> &adj){
+void percolation_bonds(vector<pair <int,int> > &adj, float q){
 	default_random_engine generator;
 	bernoulli_distribution distribution(q);
 	
@@ -59,16 +61,16 @@ int main(){
 	int n; // num de nodes
 	cin >> n >> endl;
 	
-	int q; // probabilitat que node o aresta falli
+	float q; // probabilitat que node o aresta falli
 	cin >> q >> endl;
 	
 	list<int> nodes = nodes(n); // llista de nodes
 
-	vector<pair<int,int>> adj;
+	vector< pair<int,int> > adj;
 	read_graph(adj); // adjacències del graf
 	
-	if(tipus == "sites") percolation_sites(adj,nodes);
-	else if(tipus == "bonds") percolation_bonds(adj);
+	if(tipus == "sites") percolation_sites(adj,nodes, q);
+	else if(tipus == "bonds") percolation_bonds(adj, q);
 		
 	print_graph(adj);
 }
